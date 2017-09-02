7926b852b3e09be3c853358ef5dc8f2ed1510346
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2017-09-02 19:23:27 -0300

    Finalizando register, e melhorando organizacao dos arquivos

--- server.js
+++ server.js
@@ -3,47 +3,30 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var Message = mongoose.model('Message',  {
-  msg: String
-});
 
-//Parse to response json
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
+
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-//allow cors
-app.use(function(req,res,next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Aythorization');
-  next();
-});
+//Requests
+app.get('/api/message', message.get);
 
-//Get request
-app.get('/api/message', GetMessages);
+app.post('/api/message',checkAuthenticated, message.post);
 
-//make a post
-app.post('/api/message', function(req,res){
-  console.log(req.body);
+app.post('/auth/register', auth.register);
 
-  var message = new Message(req.body);
-  message.save();
+//Connection
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
+        console.log("we are connected to mongo");
+    }
+})
 
-  res.status(200);
-});
-
-function GetMessages(req,res){
-  Message.find({}).exec(function(err,result){
-    res.send(result);
-  });
-};
-
-//connect to mongodb
-mongoose.connect('mongodb://localhost:27017/test', function(err,db){
-  if(!err){
-    console.log('We are connected to mongo');
-  }
-});
-
-//listen the server
-var server = app.listen(5000, function(){
-  console.log('listening on port ', server.address().port);
-});
+var server = app.listen(5000, function () {
+    console.log('listening on port ', server.address().port)
+})
